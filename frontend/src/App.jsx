e9b9265250f69cc412257a8f6b800d36a16304bf
Brijesh149-it <24ituos149@ddu.ac.in>
2026-09-14 22:29:08 +0530

    successfully fetch transaction data in UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [data, setData] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const getData = async () => {
+        const response = await axios.get(
+            'http://localhost:8080/api/transactions',
+        );
+        setData(response.data);
+    };
+    return (
+        <div className="w-full h-screen">
+            <h1 className="text-black text-2xl h-fit w-full text-center">
+                Transaction Details
+            </h1>
+            <button
+                className="border-2 border-black rounded-2xl my-5 w-fit px-5"
+                onClick={getData}
+            >
+                Get details
+            </button>
 
-export default App
+            <div className="mx-10">
+                <table className="w-full border-2 text-center p-5">
+                    <tr className="border-2 *:border-black *:p-2 *:text-center w-full *:h-fit">
+                        <th>Sr.</th>
+                        <th>Description</th>
+                        <th>Amount</th>
+                        <th>Type</th>
+                        <th>Transaction Date</th>
+                    </tr>
+
+                    {data.map((transaction) => (
+                        <tr
+                            className="*:p-2 *:text-center w-full *:h-fit "
+                            key={transaction.id}
+                        >
+                            <td>{transaction.id}</td>
+                            <td>{transaction.description}</td>
+                            <td>{transaction.amount.toFixed(2)}</td>
+                            <td>{transaction.transactionDate}</td>
+                            <td>{transaction.type}</td>
+                        </tr>
+                    ))}
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default App;
